905c895b40e74ee4e8e3136633a877ce44986074
andr3i1010 <andrei@andrei1010.me>
2025-03-13 15:14:50 +0000

    hope its fixed now.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-# Check required environment variables
 : "${DATABASE_URL:?Missing DATABASE_URL}"
-: "${POLARLEARN_URL:?Missing POLARLEARN_URL}"
 : "${ALLOW_EVERYONE_ON_DEV:?Missing ALLOW_EVERYONE_ON_DEV}"
 : "${AUTH_GOOGLE_ID:?Missing AUTH_GOOGLE_ID}"
 : "${AUTH_GOOGLE_SECRET:?Missing AUTH_GOOGLE_SECRET}"
@@ -10,7 +8,7 @@
 : "${AUTH_SECRET:?Missing AUTH_SECRET}"
 : "${NEXT_PUBLIC_URL:?Missing NEXT_PUBLIC_URL}"
 
-export AUTH_URL="${NEXT_PUBLIC_URL}"
+export AUTH_URL=$NEXT_PUBLIC_URL
 
 echo "Starting app..."
 # Run prisma db push first then start the app
