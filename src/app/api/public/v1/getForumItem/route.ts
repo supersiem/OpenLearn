7b4afc3fdaf324a365dcfa758aa44d3cab847d2f
Siem <vankeulensiem@gmail.com>
2025-05-30 21:11:31 +0200

    add getForumItem en getForumItemReplys

---
+++ src/app/api/public/v1/getForumItem/route.ts
@@ -0,0 +1,59 @@
+"use server";
+import { NextResponse } from 'next/server';
+import { prisma } from '@/utils/prisma';
+
+export async function GET(request: Request) {
+    // Boilerplate code start hier
+    const key = request.headers.get('token');
+
+    if (!key) {
+        return NextResponse.json({ error: 'Geen token opgegeven' }, { status: 400 });
+    }
+    // check of de token geldig is
+    const bot = await prisma.bot.findUnique({
+        where: { token: key },
+    });
+    if (!bot) {
+        return NextResponse.json({ error: 'Bot niet gevonden of token ongeldig' }, { status: 404 });
+    }
+
+    if (bot.resetToken && bot.resetToken < new Date()) {
+        return NextResponse.json({ error: 'Token is verlopen, vraag een nieuwe aan' }, { status: 401 });
+    }
+    if (!bot) {
+        return NextResponse.json({ error: 'Bot niet gevonden of token ongeldig' }, { status: 404 });
+    }
+    if (bot.resetToken && bot.resetToken < new Date()) {
+        return NextResponse.json({ error: 'Token is verlopen, vraag een nieuwe aan' }, { status: 401 });
+    }
+    // Set initial reset limit if it doesn't exist or has expired
+    const currentTime = new Date();
+    if (!bot.resetLimit || bot.resetLimit < currentTime) {
+        const nextResetTime = new Date();
+        nextResetTime.setHours(nextResetTime.getHours() + 24);
+
+        await prisma.bot.update({
+            where: { token: key },
+            data: { resetLimit: nextResetTime, limit: 100 },
+        });
+
+        // Update local bot object
+        bot.resetLimit = nextResetTime;
+        bot.limit = 100;
+    }
+    if (!bot.limit || bot.limit <= 0) {
+        return NextResponse.json({ error: 'Bot heeft geen limiet of limiet is bereikt, je kan weer verzoeken doen op ' + bot.resetLimit }, { status: 403 });
+    }
+    bot.limit -= 1;
+    await prisma.bot.update({
+        where: { token: key },
+        data: { limit: bot.limit },
+    });
+    const id = request.headers.get('id')
+    if (!id) {
+        return NextResponse.json({ error: 'er is geen id gegeven', status: 400 });
+    }
+    const forumItem = await prisma.forum.findUnique({where: { post_id: id}});
+    return NextResponse.json({forumItem,  status: 200 });
+
+}
