bf60a5a1ea45c48ca1829eff27230d2ce6c4f18a
Siem <vankeulensiem@gmail.com>
2026-01-17 22:12:56 +0100

    fix i18n

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import nlTranslation from "./locales/nl.json";
-import enTranslation from "./locales/en.json";
+// import enTranslation from "./locales/en.json";
 
 export function initI18n(lang: string = "nl") {
   const lng = lang || "nl";
@@ -12,7 +12,7 @@ export function initI18n(lang: string = "nl") {
       fallbackLng: "nl",
       resources: {
         nl: nlTranslation,
-        en: enTranslation,
+        // en: enTranslation,
       },
       interpolation: { escapeValue: false },
       react: { useSuspense: false },
