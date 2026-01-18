af1b08dcc2cc5f5f5e6dd45c92517e286d4fd25d
andr3i1010 <andrei.k@polarnl.org>
2026-01-18 12:02:47 +0100

    a

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,4 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes"
-import { flatRoutes } from "@react-router/fs-routes";;
+import { type RouteConfig, index } from "@react-router/dev/routes"
+import { flatRoutes } from "@react-router/fs-routes"
 
 export default [index("routes/home.tsx"), ...(await flatRoutes({ ignoredRouteFiles: ["routes/home.tsx"], })),] satisfies RouteConfig;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,18 +1,12 @@
 import i18n from "i18next";
 import Button1 from "~/components/Button1";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [loginLabel, setLoginLabel] = useState("login");
-
-  useEffect(() => {
-    setLoginLabel(i18n.t("loginRelated:login"));
-  }, []);
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
       <Button1 redirectTo="/auth/login">
-        {loginLabel}
+        {i18n.t("loginRelated:login")}
       </Button1>
     </div>
   );
