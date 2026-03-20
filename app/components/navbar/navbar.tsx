6ec2118021f89f56038f7c3ac2cd2cbf6e5bf0f6
lunarash0 <eamonhwatson@icloud.com>
2026-03-20 22:54:30 +0100

    logo knop navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import { Button } from "../button/button";
 import { useNavigate } from "react-router";
 import { motion } from "motion/react";
+import logo from "~/../public/logo_outline.svg";
 import "./navbar.css";
 
 export const Navbar = () => {
@@ -8,11 +9,12 @@ export const Navbar = () => {
     return <nav>
         {/* oh ja ik vind animations leuk. */}
         <motion.div
-            className="flex flex-row justify-left items-center gap-3"
+            className="flex flex-row justify-left items-center gap-3 translate-x-6 translate-y-6"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
         >
+            <img src={logo} alt="Logo" className="logo" onClick={() => navigate('/')}/>
             <div className={'navbar'}>
                 <Button variant='secondary' onClick={() => { navigate('/app') }}>Home</Button>
                 <Button variant='secondary' onClick={() => { navigate('/app/forum') }}>Forum</Button>
